d98a0706f0f3f8a68866029d4986b3e37ad9f2eb
Chavot <ukuharm@gmail.com>
2022-03-25 17:14:09 +0200

    add template

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/Chavot/docweb1.git master:gh-pages
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
